2e11a9b9238ceb95dd8af74a0f60f2795fa55691
Nevio Vesic <nevio.vesic@gmail.com>
2015-11-03 08:32:53 +0100

    Removing config as it's just way too much

--- config/vars.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2015 The Golang.hr Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
-// Package config ...
-package config
-
-var ()
